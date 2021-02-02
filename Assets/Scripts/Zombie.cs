d53c662afc045a56675031247534b91a91c633c2
Viktor Rüdén <viktor.ruden2@gmail.com>
2021-02-02 16:01:07 +0100

    zombie is the parent class

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -4,57 +4,9 @@ using UnityEngine;
 
 public class Zombie : MonoBehaviour
 {
-    [SerializeField]
+    
     public float health = 100;
-    [SerializeField]
-    public Transform Player;
-    [SerializeField]
-    float MoveSpeed = 4;
-    [SerializeField]
-    float MaxDist = 10;
-    [SerializeField]
-    float MinDist = 3;
-    protected Rigidbody rb;
-
-    float dmgTimer = 0;
-    [SerializeField] private LayerMask zombielayer;
-    [SerializeField] private LayerMask survivorLayer;
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    void Update()
-    {
-        transform.LookAt(Player);
-        
-        RaycastHit hit;
-        Physics.Raycast(transform.position, transform.forward, out hit, 1000);
-        
-        if (hit.transform.GetComponent<Survivor>() || hit.transform.GetComponent<Zombie>())
-        {
-            rb.MovePosition(transform.position + transform.forward * MoveSpeed * Time.deltaTime);
-        }
-        else
-        {
-            rb.MovePosition(transform.position + new Vector3(1,0,0) * MoveSpeed * Time.deltaTime);
-        }
-        
-        if (Vector3.Distance(transform.position, Player.position) <= MinDist)
-        {
-            if (dmgTimer <= 0)
-            {
-                Player.GetComponent<Survivor>().takeDamage(100);
-                dmgTimer = 2;
-            }
-            else
-            {
-                dmgTimer -= Time.deltaTime;
-            }
-        }
-    }
-
-
+  
     public void takeDamage(int damage)
     {
 
