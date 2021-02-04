83da158e879995f6409bd559b015895521c1cdff
Arthur <Geleminecrafer+Git@gmail.com>
2021-02-04 17:02:01 +0100

    giveme level2

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,24 +6,46 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    // singleton stuff
+    private static LevelManager _instance;
+    public static LevelManager Instance => _instance;
+    
+    
+    private int loadedSceneIndex = 0;
 
+    private void Awake()
+    {
+        // singleton stuff
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadNextScene()
+    {
+        LoadScene(loadedSceneIndex+1);
+    }
     public void LoadScene(string name)
     {
-       
         SceneManager.LoadScene(name);
+        loadedSceneIndex = SceneManager.GetActiveScene().buildIndex;
     }
     public void LoadScene(int index)
     {
+        loadedSceneIndex = index;
         SceneManager.LoadScene(index);
     }
     public void ExitGame()
     {
         Application.Quit();
     }
+    
 }
